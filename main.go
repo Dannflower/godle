0b009e800c1bf7622c01161efa61e708f1f4a928
Eric Ostrowski <eric.ostrowski@live.com>
2022-02-12 17:07:26 -0600

    Add menu option selection

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	printTitle()
+	printMenu()
+
+}
+
+func printTitle() {
 
 	title := "  _____           _ _       \n" +
 		" / ____|         | | |      \n" +
@@ -12,4 +22,42 @@ func main() {
 		" \\_____|\\___/ \\__,_|_|\\___| \n"
 
 	fmt.Println(title)
+}
+
+// Prints the menu of game options.
+func printMenu() {
+
+	fmt.Println("Options\t\tKey")
+	fmt.Println("-------\t\t---")
+	fmt.Println("New Game\t n")
+	fmt.Println("Rules\t\t r")
+	fmt.Println("Quit\t\t q")
+	fmt.Println()
+
+	var input string
+	fmt.Print("Command: ")
+	fmt.Scanln(&input)
+
+	switch input {
+	case "n":
+		// Start new game
+	case "r":
+		// Display rules, loop back to start of input
+	case "q":
+		fmt.Println("Thanks for playing!")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid option.")
+		// loop back to start of input
+	}
+
+}
+
+func play() {
+
+}
+
+func selectWord() string {
+
+	return ""
 }
